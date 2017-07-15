940651d47aa719275075a4f60ddb052f1928bf73
Ederlo Rodrigo <rodrigo@personaliza.com.br>
2017-07-15 16:51:23 -0300

    fix Sms

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # My project's README
 =======
+
+use Aws\Sns\SnsClient;
+use rederlo\Sns\Sms\SnsSms;
+
+$sns = new SnsClient([
+    'credentials' => [
+        'key' => S3_KEY,
+        'secret' => S3_SECRET_KEY,
+    ],
+    'region' => REGION, // < your aws from SNS Topic region
+    'version' => 'latest'
+]);
+
+
+$args = [
+    "SenderID" => "",
+    "SMSType" => "Transactional",
+    "Message" => utf8_encode(utf8_decode("teste")),
+    "PhoneNumber" => "+55phone"
+];
+
+$sms = new SnsSms($sns);
+$sms->send($args);
--- src/Sms/SnsSms.php
+++ src/Sms/SnsSms.php
@@ -1,7 +1,6 @@
 <?php
 namespace rederlo\Sns\Sms;
 
-use Aws\Sns\SnsClient;
 use rederlo\Sns\Http\HttpResponse;
 
 /**
@@ -18,27 +17,13 @@ class SnsSms
     /**
      *
      */
-    public function __construct()
+    public function __construct(\Aws\Sns\SnsClient $sns)
     {
-        $this->Sns = new SnsClient([
-            'credentials' => [
-                'key' => S3_KEY,
-                'secret' => S3_SECRET_KEY,
-            ],
-            'region' => REGION, // < your aws from SNS Topic region
-            'version' => 'latest'
-        ]);
+        $this->Sns = $sns;
     }
 
-    public function send($phone, $text)
+    public function send(array $args)
     {
-        $args = [
-            "SenderID" => "Sage",
-            "SMSType" => "Transactional",
-            "Message" => utf8_encode(utf8_decode($text)),
-            "PhoneNumber" => $phone
-        ];
-
         $info = $this->Sns->publish($args);
         $response = new HttpResponse();
         $response->setStatusCode((int)$info['@metadata']['statusCode']);
